1166b539462686e9a1a30f56aba5ccc99ac57655
Amanda Winkles <amandawinkles@gmail.com>
2020-07-13 17:27:04 -0500

    initial setup

---
+++ client/index.js
@@ -0,0 +1,41 @@
+const artists = [
+  {
+    name: 'letha wilson',
+    imageURL: 'https://www.lethaprojects.com/visuals/images/outdoors/ghostofatree-right-view.jpg'
+  },
+  {
+    name: 'genesis baez',
+    imageURL: 'http://media.virbcdn.com/cdn_images/resize_1600x1600/23/ac1718b745965aed-Baez_1.jpg'
+  },
+  {
+    name: 'sarah-louise barbett',
+    imageURL: 'https://aisselles.files.wordpress.com/2010/12/img_0016.jpg?w=1104'
+  },
+  {
+    name: 'jessica halonen',
+    imageURL: 'http://www.jessicahalonen.com/files/gimgs/44_spliced-branch-ball-1.jpg'
+  },
+  {
+    name: 'laura owens',
+    imageURL: 'https://www.owenslaura.com/wp-content/uploads/2013/01/HQ-16LO9359P-Untitled.jpg'
+  },
+  {
+    name: 'deborah roberts',
+    imageURL: 'https://i0.wp.com/conflictofinteresttx.com/wp-content/uploads/2017/07/The-Power-dance-30x22-2017.jpeg?w=469'
+  },
+  {
+    name: 'ana esteve llorens',
+    imageURL: 'http://www.anaestevellorens.com/projects/project6/07%20Quasy%20Infinite.jpg'
+  },
+  {
+    name: 'jackie furtado',
+    imageURL: 'https://s3.amazonaws.com/artfare-production-mobile/Artworks/Images/Image-1/image-1-d67b0616-077b-4fee-97be-275e4dde6107.jpg'
+  },
+  {
+    name: 'viviane sassen',
+    imageURL: 'https://www.vivianesassen.com/site/assets/files/2940/umbra_nab_vs_3942.0x1500.jpg'
+  }
+];
+
+//claire falkenberg
+//https://clairefalkenberg.com/floating
